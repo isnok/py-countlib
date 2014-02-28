2d2cedad316c67b849a397e3e7a4a714232f88b1
Konstantin Martini <k@tuxcode.org>
2014-02-28 01:02:05 +0100

    TestResults(failed=4, attempted=11)

--- countlib/xcount.py
+++ countlib/xcount.py
@@ -2,10 +2,51 @@
 from collections import Counter
 from pivot import PivotCounter
 
+from operator import itemgetter
+from heapq import nlargest, nsmallest
+
 class ExtremeCounter(Counter):
+    """ Buffed Counter class. Extreme usefulness to be expected.
+
+    >>> ExtremeCounter('zyzygy')
+    ExtremeCounter({'g': 1, 'z': 2, 'z': 3})
+    >>> x = ExtremeCounter("yay? nice!! this thing works!!")
+    >>> x.most_common_counts(1)
+    [(' ', 4), ('!', 4)]
+    >>> x.most_common_counts(1, reverse=True)
+    [('a', 1), ('c', 1), ('e', 1), ('g', 1), ('k', 1), ('o', 1), ('w', 1), ('r', 1), ('?', 1)]
+
+    """
+
+    def most_common(self, n=None, count_func=None, inverse=False):
+        """ List the n most common elements and their counts from the most
+            common to the least.  If n is None, then list all element counts.
+            The way counting is done can be customized via the count_func
+            argument, that is passed to the sorting as the key function.
+
+
+        >>> p = ExtremeCounter('abracadabra!')
+        >>> p.most_common(3)
+        [('a', 5), ('b', 2), ('r', 2)]
+        >>> p.most_common(2, reverse=True)
+        [('!', 1), ('c', 1)]
+        >>> p.most_common(2, count_func=lambda i: -i[1])
+        [('!', 1), ('c', 1)]
+        >>> p.most_common(2, count_func=lambda i: -i[1], inverse=True)
+        [('!', 1), ('c', 1)]
+
+        """
+        if count_func is None:
+            count_func = itemgetter(1)
+        if n is None:
+            return sorted(self.iteritems(), key=count_func, reverse=not inverse)
+        if inverse:
+            return nsmallest(n, self.iteritems(), key=count_func)
+        else:
+            return nlargest(n, self.iteritems(), key=count_func)
+
 
     def iter_most_common_counts(self, limit, *args, **kw):
-        """ like most_common() but limit is applied to values (counts) """
         for thing, count in self.most_common(*args, **kw):
             if "last_count" in locals():
                 if count != last_count:
@@ -18,7 +59,8 @@ class ExtremeCounter(Counter):
             yield (thing, count)
 
     def most_common_counts(self, *args, **kw):
-        """ get the most common counts as a list of (elem, cnt) tuples """
+        """ get the most common counts as a list of (elem, cnt) tuples.
+            like most_common() but limit is applied to values (counts) """
         return list(self.iter_most_common_counts(*args, **kw))
 
     def pivot(self):
@@ -42,6 +84,24 @@ class ExtremeCounter(Counter):
         """ Init a constant Counter. Useful for Counter arithmetic. """
         return cls(dict.fromkeys(iterable, v))
 
+    def __repr__(self):
+        """ Output like defaultdict or other dict variants.
+            Of course ExtremeCounters can be copy-pasted.
+
+        >>> ExtremeCounter('bumm')
+        ExtremeCounter({'b': 1, 'm': 2, 'u': 1})
+        >>> ExtremeCounter({'b': 1, 'm': 2, 'u': 1})
+        ExtremeCounter({'b': 1, 'm': 2, 'u': 1})
+
+        """
+        def stable_output():
+            for elem, count in self.iteritems():
+                yield '%r: %r' % (elem, count)
+
+        if not self:
+            return '%s()' % self.__class__.__name__
+        items = ', '.join(sorted(stable_output()))
+        return '%s({%s})' % (self.__class__.__name__, items)
 
 class ListCounter(Counter):
     """ A Counter that uses list instead of int objects to count. """
@@ -49,9 +109,12 @@ class ListCounter(Counter):
     def __missing__(self, key):
         return []
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+    import doctest
+    print doctest.testmod()
+
     x = ExtremeCounter("yay? nice!! this thing works!")
-    print x.most_common_counts(1)
     x.update("etsttseststttsetsetse ")
     print x.most_common_counts(1)
     print x.most_common_counts(5)
