de1665b52b4bdd9cb0cb5000aa8c34bc6485e8e2
Konstantin Martini <k@tuxcode.org>
2014-03-02 10:30:07 +0100

    181 test pass

--- src/countlib/test_count_inplace_ops.py
+++ src/countlib/test_count_inplace_ops.py
@@ -28,3 +28,67 @@ def test_subtract(TestCounter):
     c.subtract(w=-6)
     assert c['w'] == 5
 
+def test__iadd__(cnt_abc):
+    cnt_abc += 2
+    assert cnt_abc == Counter("abc" * 3)
+
+def test__isub__(cnt_abc):
+    assert cnt_abc
+    cnt_abc -= 2
+    assert cnt_abc
+    assert not +cnt_abc
+
+def test__imul__(cnt_abc):
+    cnt_abc *= 4
+    assert cnt_abc == Counter("abc" * 4)
+
+def test__idiv__(TestCounter, cnt_abc):
+    cnt_abc /= 2
+    assert cnt_abc == TestCounter("abc") - 1
+
+def test__irshift__(TestCounter, cnt_abc):
+    cnt_abc >>= 2
+    assert cnt_abc == TestCounter("abc") - 1
+
+def test__ilshift__(TestCounter, cnt_abc):
+    cnt_abc <<= 2
+    assert cnt_abc == TestCounter("abc") * 4
+
+def test__imod__(cnt_ab2):
+    t = 1 % cnt_ab2
+    assert t
+    assert set(t.values()) == set([0,1])
+    assert Counter("abcabc") % +(Counter("abcabc") % cnt_ab2)
+
+def test__ipow__(cnt_ab2):
+    tmp = cnt_ab2.copy()
+    cnt_ab2 **= 3
+    assert cnt_ab2
+    tmp **= 2
+    assert tmp
+    for i in tmp:
+        assert tmp[i] <= cnt_ab2[i]
+
+def test___ior__(cnt_ab2, cnt_abc):
+    tmp = cnt_abc.copy()
+    cnt_abc |= cnt_ab2
+    assert cnt_abc != tmp
+    tmp = cnt_ab2.copy()
+    cnt_ab2 |= cnt_abc * 3
+    assert cnt_ab2 != tmp
+
+def test___iand__(cnt_ab2, cnt_abc):
+    tmp = cnt_ab2.copy()
+    cnt_ab2 &= cnt_abc
+    assert cnt_ab2 != tmp
+    tmp = cnt_abc.copy()
+    cnt_abc &= cnt_ab2 - 2
+    assert tmp != cnt_abc
+
+def test___ixor__(cnt_abc):
+    tmp = cnt_abc.copy()
+    cnt_abc ^= 1
+    assert not tmp == cnt_abc
+    assert cnt_abc
+    assert not +cnt_abc
+
